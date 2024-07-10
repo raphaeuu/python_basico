8fec567f94e54e0482f35b4a01f8d2446b93a2dc
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-10 10:23:28 -0300

    Calculadora de Índice de Massa Corpórea

--- imc.py
+++ imc.py
@@ -1,8 +1,19 @@
 nome_usuario = input('Informe seu nome: ')
-peso_usuario = float(input('Insira seu peso:'))
 altura_usuario = float(input('Insira sua altura: '))
-calculo_usuario = round(peso_usuario / altura_usuario ** 2)
+peso_usuario = float(input('Insira seu peso: '))
+calculo_usuario = round(peso_usuario / altura_usuario ** 2, 2)
 
-print(f'{nome_usuario}, seu IMC é: ', calculo_usuario)
+print(f'{nome_usuario}, seu Índice de Massa Corpórea (IMC) é: ', calculo_usuario)
 
-
+if calculo_usuario < 18.5:
+    print(f'{nome_usuario}, você está em um nível de magreza elevado, com grau zero (0) de obesidade.')
+elif calculo_usuario > 18 and calculo_usuario == 25:
+    print(f'{nome_usuario}, você está em um nível normal, com grau 0 (zero) de obesidade.')
+elif calculo_usuario == 25 and calculo_usuario < 29.9:
+    print(f'{nome_usuario}, você está em um nível de sobrepreso, com grau  I (um) de obesidade.')
+elif calculo_usuario == 30 and calculo_usuario < 39.9:
+    print(f'{nome_usuario}, você está em um nível de obesidade, com grau dois II (dois) de obesidade.')
+elif calculo_usuario > 40:
+    print(f'{nome_usuario}, você está em um nível de obesidade grave, com grau III (três) de obesidade.')
+else:
+    print('Tente novamente inserindo seu peso e altura corretamente, usando número e se caso precisar, separar com ponto (.) .')
