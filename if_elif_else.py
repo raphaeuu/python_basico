c102e14e7d38802ae4dcb8d8f5a970203a8d535b
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-12 10:48:48 -0300

    Exercício de comparação / if - elif - else

--- if_elif_else.py
+++ if_elif_else.py
@@ -2,17 +2,18 @@ usuario_1 = input('Insira o nome do jogador 1: ')
 usuario_2 = input('Insira o nome do jogador 2: ')
 valor_1 = int(input(f'{usuario_1}, digite o primeiro valor: '))
 valor_2 = int(input(f'{usuario_2}, digite o segundo valor: '))
+print('--------------------------------------')
 print('\n')
 
 
 if valor_1 > valor_2:
     print(f'{usuario_1}, seu valor é maior. ({valor_1})')
     print('\n')
-    print(f'Valor escolhido por {usuario_2}: {valor_2}')
+    print(f'Valor escolhido por {usuario_2}: {valor_2} é menor. ')
 elif valor_2 > valor_1:
     print(f'{usuario_2}, seu valor é maior. ({valor_2})')
     print('\n')
-    print(f'Valor escolhido por {usuario_1}: ({valor_1})')
+    print(f'Valor escolhido por {usuario_1}: ({valor_1}) é menor.')
 else:
     print('Digite um valor válido e tente novamente, obrigado!')
 
