d08f1d2ea7fb31dfe090b1de30372f9b4db1baf7
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-17 11:09:48 -0300

    Operador AND e OR

--- operador_and_or.py
+++ operador_and_or.py
@@ -1,6 +1,6 @@
 entrada = input('[E] Entrar [S] Sair: ')
 senha_digitada = input('Insira a senha: ')
-senha_permitida = '123456'
+senha_permitida = '1234567'
 
 if (entrada == 'E' or 'e') and senha_digitada == senha_permitida:
     print('Entrar')
