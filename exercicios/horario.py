56320922e51ca76eb21bb3bd874f41ca172b97b9
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-19 14:19:50 -0300

    Exercício - Horário do usuário

---
+++ exercicios/horario.py
@@ -0,0 +1,23 @@
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+
+"""
+
+#Exercício 2
+
+try:
+    nome = input('Me diga o seu nome: ')
+    horario = int(input('Me informe seu horário: '))
+
+    if horario <= 11:
+        print(f'Bom dia, {nome}!')
+    elif horario >= 12 and horario <= 17:
+        print(f'Boa tarde, {nome}!')
+    elif horario >= 18 and horario <= 23:
+        print(f'Boa noite, {nome}!')
+    else:
+        print(f'{nome}, hora inválida. Digite um valor entre 0 à 23.')
+except ValueError:
+    print('Informe apenas 2 (dois) dígitos.')
