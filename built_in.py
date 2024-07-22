5f443fe8dbd26b8ed513d1282573a1b478bbded6
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-22 10:16:26 -0300

    Tipos imutáveis

---
+++ built_in.py
@@ -0,0 +1,9 @@
+# Conversa/ tipos built-in, documentação, tipos imutáveis, métodos de string.
+# Imutáveis: str, int, float, bool
+
+string = 'Raphael Henrique'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# string[3] = 
+# print(string)
+# print(outra_variavel)
+# print(string.zfill(10))
