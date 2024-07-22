0cf0b9500e6a72e31f79c7dbbf5c57b383904783
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-22 11:53:01 -0300

    While e Continue

---
+++ while_continue.py
@@ -0,0 +1,20 @@
+contador = 0
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print(f'Não vou mostrar o {contador}.')
+        continue
+
+    print(contador)
+
+    if contador >= 10 and contador <= 27:
+        print(f'Não vou mostrar o {contador}')
+        continue
+
+    if contador == 40:
+        print(f'Não vou mostrar o {contador}')
+        break
+
+print('Acabou!')
