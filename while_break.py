44bce547bf676dd5b324bd71476f8f227c7e1cc5
raphael.monteiro <raphahenrique2808@gmail.com>
2024-07-22 11:07:48 -0300

    While - condições em detalhes

---
+++ while_break.py
@@ -0,0 +1,31 @@
+"""
+Repetições
+While (Enquanto)
+Executa uma ação enquanto uma condição for verdadeira.
+
+"""
+# condicao = True
+
+# while condicao:
+#     nome = input('Qual o seu nome: ')
+#     print(f'Seu nome é {nome}')
+
+#     if nome == 'Sair':
+#         break
+
+# print('Acabou!')
+
+
+# contador = 0
+
+# while contador < 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou!')
+
+# contador = 0
+
+
+# contador += 2
+# print(contador)
