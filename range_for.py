607dff46bd7226cedda0ff14ddf08290e997461d
raphael.monteiro <raphahenrique2808@gmail.com>
2024-08-16 10:14:46 -0300

    For e Range

--- range_for.py
+++ range_for.py
@@ -16,11 +16,26 @@ print(texto.__next__())
 
 """
 
-texto = 'Luiz' # iterável
-iterador = iter(texto) # iterator
+# texto = 'Luiz' # iterável
+# iterador = iter(texto) # iterator
 
-while True:
-    try:
-        print(next(iterador))
-    except StopIteration:
-        print()
+# while True:
+#     try:
+#         print(next(iterador))
+#     except StopIteration:
+#         break
+
+
+for i in range(3):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break
+
+    for j in range(1, 3):
+        print(i, j)
+else:
+    print('For completo com sucesso!')
