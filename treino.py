d596af6a1cfd43bb3055f5bbcc0dda35d22e4cab
raphael.monteiro <raphahenrique2808@gmail.com>
2024-11-22 09:47:25 -0300

    Robô - Bloco de Notas

---
+++ treino.py
@@ -0,0 +1,36 @@
+import pyautogui
+import time
+import subprocess
+
+
+def abrir_Windows():
+    pyautogui.press('win')
+    print('Barra de pesquisa aberta.')
+
+def abrir_bloco_notas():
+    pyautogui.write('bloco de notas', interval= 0.1)
+    pyautogui.press('enter')
+    time.sleep(1)
+
+# def criar_arquivo():
+#     pyautogui.hotkey
+
+# def criar_arquivo_novo():
+#     pyautogui.hotkey('ctrl', 'n')
+#     print('Arquivo criado.')
+#     time.sleep(2)
+
+
+def escrever_nome():
+    pyautogui.write('Seja bem vindo, Raphael! Faca sua lista abaixo: ')
+    print('Texto inserido.')
+    time.sleep(1)
+
+def salvar_arquivo():
+    pyautogui.press('arch')
+    pyautogui.press('enter')
+
+abrir_Windows()
+abrir_bloco_notas()
+# criar_arquivo_novo()
+escrever_nome()
