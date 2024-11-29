2fc04842747fb82bae4bdd09e836491a6e6210a0
raphael.monteiro <raphahenrique2808@gmail.com>
2024-11-29 11:17:15 -0300

    Exercício de fixação

--- exercicios/adivinhar_numero.py
+++ exercicios/adivinhar_numero.py
@@ -1,10 +1,27 @@
 import random
 numero_secreto = random.randint(1, 10)
-palpite = int(input("Adivinhe o número entre 1 e 10: "))
 
-while palpite != numero_secreto:
-    print("Errado! Tente novamente.")
-    palpite = int(input("Adivinhe o número entre 1 e 10: "))
-    
-print(f"Parabéns! O número secreto é ({numero_secreto}).")
+while True:
 
+    try:
+        palpite = int(input("Adivinhe o número entre 1 e 10: "))
+        print('\n')
+
+        if palpite < 1 or palpite > 10:
+            print('Por favor, digite um número entre 1 e 10.')
+            print('\n')
+            continue
+        
+        elif palpite == numero_secreto:
+             print(f'Parabéns! Seu palpite está correto. O número correto é: ({numero_secreto})')
+             break
+        
+        elif palpite > numero_secreto:
+            print(f'{palpite} é maior do que o número secreto.')
+           
+        else:
+            print(f'{palpite} é menor do que o número secreto.')
+
+    except ValueError: 
+        print('Erro. Tente novamente inserindo um número inteiro!')
+        print('\n')
